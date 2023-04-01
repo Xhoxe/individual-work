201a436f39041ad338be73f6668dff4b366ce56f
evgenijkrotko8 <evgenijkrotko8@gmail.com>
2023-04-01 17:30:31 +0300

    done IW 3 (Krotko)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import sqlite3
-import config
+import os
+
+import config #містить токен бота (https://t.me/FP_individual_project_Bot)
 
 
 bot = Bot(token=config.TOKET)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 @dp.message_handler(commands=['start'])
@@ -100,16 +103,156 @@ async def process_callback_query(callback_query: types.CallbackQuery):
     conn.close()
     
     
-#блок індивідуальне завдання N3 (Кротко)
+######## Блок індивідуальне завдання N3 (Кротко) ########
 
+#меню практичних
+@dp.callback_query_handler(lambda query: query.data == "practical_matirials_manage")
 async def process_pr_query(callback_query: types.CallbackQuery):
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(types.InlineKeyboardButton(text="Завантажити нову ПР", callback_data="practical_matirials_add"))
-    keyboard.add(types.InlineKeyboardButton(text="Змінити існуючу ПР", callback_data="practical_matirials_change"))
-    keyboard.add(types.InlineKeyboardButton(text="Видалити існуючу ПР", callback_data="practical_matirials_remove"))
+    keyboard.add(types.InlineKeyboardButton(text="Завантажити нову ПР", callback_data="practical_matirials_manage_add"))
+    keyboard.add(types.InlineKeyboardButton(text="Видалити існуючу ПР", callback_data="practical_matirials_manage_remove"))
+    keyboard.add(types.InlineKeyboardButton(text="Видалити ПР за конкретною темою", callback_data="practical_matirials_manage_themeRemove"))
     await bot.send_message(chat_id=callback_query.message.chat.id, text="Практичні роботи:", reply_markup=keyboard)
+
+
+#блок додавання нового ПР
+@dp.callback_query_handler(lambda query: query.data == "practical_matirials_manage_add")
+async def process_pr_query_add(callback_query: types.CallbackQuery):
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM themes")
+    themes = cur.fetchall()
+    keyboard = types.InlineKeyboardMarkup()
+    for theme in themes:
+        keyboard.add(types.InlineKeyboardButton(text=theme[1], callback_data=f"practical_matirials_manage_add_theme_{theme[0]}"))
+    await bot.send_message(chat_id=callback_query.message.chat.id, text="Оберіть тему:", reply_markup=keyboard)
+    conn.close()
+
+
+# обнраємо тему та запитуємо назву
+@dp.callback_query_handler(lambda query: query.data.startswith("practical_matirials_manage_add_theme_"))
+async def process_pr_query_add(callback_query: types.CallbackQuery):
+    theme_id = callback_query.data.split("practical_matirials_manage_add_theme_")[1]
+    await bot.send_message(chat_id=callback_query.message.chat.id, text="Введіть назву практичної роботи:")
+    await dp.current_state().set_state('waiting_for_practical_material_title')
+    await dp.current_state().update_data(theme_id=theme_id)
+
+
+# вводимо назву ПР та запитуємо файл
+async def process_pr_query_add_title(message: types.Message):
+    practical_material_title = message.text
+    await bot.send_message(chat_id=message.chat.id, text="Надішліть файл практичної роботи:")
+    await dp.current_state().set_state('waiting_for_practical_material_file')
+    await dp.current_state().update_data(practical_material_title=practical_material_title)
+
+
+# зберігаємо фалй та додаємо запис в БД
+async def process_pr_query_add_file(message: types.Message):
+    data = await dp.current_state().get_data()
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    file_path = os.path.join("Files", "PracticalMaterials", message.document.file_name)
+    await bot.download_file_by_id(message.document.file_id, file_path)
+    cur.execute("INSERT INTO practical_tasks (id, theme_id, title, content) VALUES (?, ?, ?, ?)", (None, data['theme_id'], data['practical_material_title'], file_path))
+    conn.commit()
+    conn.close()
+    await bot.send_message(chat_id=message.chat.id, text=f"Практична робота '{data['practical_material_title']}' додана.")
     
-###############################
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Повернутись назад до меню", callback_data="practical_matirials_manage"))
+    await bot.send_message(chat_id=message.chat.id, text=f"Повернутись до меню ПР?", reply_markup=keyboard)
+    
+    await dp.current_state().reset_state(with_data=False)
+
+
+dp.register_message_handler(process_pr_query_add_title, state='waiting_for_practical_material_title') # стадія отримання назви
+dp.register_message_handler(process_pr_query_add_file, state='waiting_for_practical_material_file', content_types=['document']) # стадія отримання файлу
+#кінець блоку додавання ПР
+
+#блок видалення ПР
+# виводимо інлайн кнопки з переліком практичних (видалення конкретної ПР)
+@dp.callback_query_handler(lambda query: query.data == "practical_matirials_manage_remove")
+async def process_pr_query_remove(callback_query: types.CallbackQuery):
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM practical_tasks")
+    pws = cur.fetchall()
+    keyboard = types.InlineKeyboardMarkup()
+    
+    for pw in pws:
+        keyboard.add(types.InlineKeyboardButton(text=pw[2], callback_data=f"practical_matirials_manage_remove_pr_{pw[0]}"))   
+         
+    await bot.send_message(chat_id=callback_query.message.chat.id, text="Оберіть практичну котру необхідно видалить:", reply_markup=keyboard)    
+    conn.close()
+    
+   
+# процес видалення ПР (видалення конкретної ПР) 
+@dp.callback_query_handler(lambda query: query.data.startswith("practical_matirials_manage_remove_pr_"))
+async def process_pr_query_remove(callback_query: types.CallbackQuery):
+    pw_id = callback_query.data.split("practical_matirials_manage_remove_pr_")[1]
+    
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM practical_tasks WHERE id = ?", (pw_id,))
+    pws = cur.fetchall()
+    
+    for pw in pws:
+        try:
+            os.remove(pw[3])
+        except:
+            pass
+        
+    cur.execute("DELETE FROM practical_tasks WHERE id = ?", (pw_id,))
+    conn.commit()
+    conn.close()
+    
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Повернутись назад до меню", callback_data="practical_matirials_manage"))
+    await bot.send_message(chat_id=callback_query.message.chat.id, text="Успішно видалено!\n Повернутись до меню ПР?", reply_markup=keyboard)
+    
+
+# видалення практичних за темами, виводим кнопки з переліком тем   
+@dp.callback_query_handler(lambda query: query.data == "practical_matirials_manage_themeRemove")
+async def process_pr_query_themeRemove(callback_query: types.CallbackQuery):
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM themes")
+    themes = cur.fetchall()
+    keyboard = types.InlineKeyboardMarkup()
+    
+    for theme in themes:
+        keyboard.add(types.InlineKeyboardButton(text=theme[1], callback_data=f"practical_matirials_manage_themeRemove_theme_{theme[0]}"))
+        
+    await bot.send_message(chat_id=callback_query.message.chat.id, text="Оберіть тему:", reply_markup=keyboard)  
+    conn.close()
+    
+
+# отримуємо тему та видаляємо всі практичні з цією темою 
+@dp.callback_query_handler(lambda query: query.data.startswith("practical_matirials_manage_themeRemove_theme_"))
+async def process_pr_query_themeRemove(callback_query: types.CallbackQuery):
+    theme = callback_query.data.split("practical_matirials_manage_themeRemove_theme_")[1]
+    
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM practical_tasks WHERE theme_id = ?", (theme,))
+    pws = cur.fetchall()
+    
+    for pw in pws:
+        try:
+            os.remove(pw[3])
+        except:
+            pass
+        
+    cur.execute("DELETE FROM practical_tasks WHERE theme_id = ?", (theme,))
+    conn.commit()
+    conn.close()
+    
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Повернутись назад до меню", callback_data="practical_matirials_manage"))
+    await bot.send_message(chat_id=callback_query.message.chat.id, text="Успішно видалено!\n Повернутись до меню ПР?", reply_markup=keyboard)
+    
+#кінець блоку видалення ПР
+############ кінець блоку N3 ###############
 
 
 if __name__ == '__main__':
