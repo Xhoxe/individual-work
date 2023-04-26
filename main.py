d457858dfec29441b0a0dd509871ee2edef1bc80
evgenijkrotko8 <evgenijkrotko8@gmail.com>
2023-04-26 15:26:10 +0300

    ...

--- main.py
+++ main.py
@@ -23,6 +23,15 @@ async def send_welcome(message: types.Message):
 
     if existing_user is None:
         await bot.send_message(chat_id=message.chat.id, text="Будь-ласка введіть своє ім'я:")
+    else:
+        #Клавіатура для виведення матеріалів та їх історія
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        theory_button = types.KeyboardButton(text="Теоретичні матеріали")
+        practice_button = types.KeyboardButton(text="Практичні роботи та тести")
+        completed_button = types.KeyboardButton(text="Завершено")
+        keyboard.add(theory_button, practice_button)
+        keyboard.add(completed_button)
+        await bot.send_message(chat_id=message.chat.id, text="Ви успішно додані до вибраної групи!", reply_markup=keyboard)
 
 
 @dp.message_handler(commands=['menu'])
@@ -46,6 +55,103 @@ async def send_welcome(message: types.Message):
         await bot.send_message(chat_id=message.chat.id, text="Меню:", reply_markup=keyboard)
 
 
+######## Блок індивідуальне завдання N7 (Ярослав) ########
+
+#Вивід даних для звичайного користувача (поверхневе, просто вивожу дані з бд, у кого по завданню перероблюйте)
+
+def all_teory_requests(user_id):
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM ready_unverified_user_tasks")
+    end = cur.fetchall()
+
+    cur.execute("SELECT * FROM theoretical_materials")
+    out = cur.fetchall()
+
+    for row in out[:]:
+
+        if any(str(row[0]) == str(e[1]).replace('read_', '') and int(user_id) == int(e[2])  for e in end):
+            print(out)
+            out.remove(row)
+
+    conn.close()
+    return out
+
+
+def all_end(user_id):
+    result = []
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM ready_unverified_user_tasks")
+    end = cur.fetchall()
+
+    cur.execute("SELECT * FROM theoretical_materials")
+    out = cur.fetchall()
+
+    for row in out[:]:
+
+        if any(str(row[0]) == str(e[1]).replace('read_', '') and int(user_id) == int(e[2])  for e in end):
+            print(row)
+            result.append(row)
+
+    conn.close()
+    return result
+
+@dp.message_handler(text="Теоретичні матеріали")
+async def all_theory_for_student(message: types.Message):
+    all_teory = all_teory_requests(message.chat.id)
+
+    if(all_teory):
+        for file in all_teory:
+            keyboard = types.InlineKeyboardMarkup()
+            keyboard.add(types.InlineKeyboardButton(text="Прочитано", callback_data="read_"+str(file[0])))   
+            await bot.send_message(chat_id=message.chat.id, text=str(file).replace(',','\n').translate(str.maketrans({'(': None, ')': None, "'": None})),reply_markup=keyboard)
+    else:
+        await bot.send_message(chat_id=message.chat.id, text="Завдань нема\n")
+
+@dp.message_handler(text="Завершено")
+async def all_end_tasks(message: types.Message):
+    end = all_end(message.chat.id)
+
+    if(end):
+        for file in end:   
+            await bot.send_message(chat_id=message.chat.id, text=str(file).replace(',','\n').translate(str.maketrans({'(': None, ')': None, "'": None})))
+    else:
+        await bot.send_message(chat_id=message.chat.id, text="Завдань нема\n")
+
+@dp.message_handler(text="Практичні роботи та тести")
+async def all_tasks(message: types.Message):
+    conn = sqlite3.connect('FP.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM practical_tasks")
+    all_tasks = cur.fetchall()
+
+    await bot.send_message(chat_id=message.chat.id, text="Практичні завдання:\n")
+
+    if(all_tasks):
+        for file in all_tasks:
+            keyboard = types.InlineKeyboardMarkup()
+            keyboard.add(types.InlineKeyboardButton(text="Прикріпити файл", callback_data="prac_"+str(file[0])))
+            await bot.send_message(chat_id=message.chat.id, text=str(file).replace(',','\n').translate(str.maketrans({'(': None, ')': None, "'": None})),reply_markup=keyboard)
+
+        cur.execute("SELECT * FROM test_tasks") #Вивід тесту некоректний, на момент добавлення, повністью нереалізовані тести, виводятся питання
+        all_tests = cur.fetchall()
+    else:
+        await bot.send_message(chat_id=message.chat.id, text="Нових практичних нема\n")
+    await bot.send_message(chat_id=message.chat.id, text="Тести:\n")
+
+    if(all_tasks):
+        for file in all_tests:
+            keyboard = types.InlineKeyboardMarkup()
+            keyboard.add(types.InlineKeyboardButton(text="Пройти", callback_data="answer_"+str(file[0])))
+            await bot.send_message(chat_id=message.chat.id, text=str(file).replace(',','\n').translate(str.maketrans({'(': None, ')': None, "'": None})),reply_markup=keyboard)
+    else:
+        await bot.send_message(chat_id=message.chat.id, text="Нових тестів нема\n")
+    conn.close()
+
+########################### Кінець ###########################
+
+
 #спрацбовує лише у випадку якщо користувача немає в БД
 @dp.message_handler()
 async def check_if_user_exists(message: types.Message):
@@ -59,6 +165,34 @@ async def check_if_user_exists(message: types.Message):
         await process_name(message)
         
     conn.close()
+    
+    
+######## Блок індивідуальне завдання N7 (Ярослав) ########
+@dp.callback_query_handler(lambda query: query.data.startswith("read_"))
+async def read_teory(callback_query: types.CallbackQuery):    
+    conn = sqlite3.connect('FP.db')
+    user_id = callback_query.from_user.id
+    cur = conn.cursor()
+    cur.execute("INSERT INTO ready_unverified_user_tasks (all_tasks, user_id) VALUES (?, ?)", (callback_query.data, user_id))
+    conn.commit()
+
+    await bot.send_message(chat_id=user_id, text="Інші не прочитані завдання\n")
+    all_teory = all_teory_requests(user_id)
+
+    if(all_teory):
+        for file in all_teory:
+            keyboard = types.InlineKeyboardMarkup()
+            keyboard.add(types.InlineKeyboardButton(text="Прочитано", callback_data="read_"+str(file[0])))   
+            await bot.send_message(chat_id=user_id, text=str(file).replace(',','\n').translate(str.maketrans({'(': None, ')': None, "'": None})),reply_markup=keyboard)
+    else:
+        await bot.send_message(chat_id=user_id, text="Завдань нема\n")
+
+
+    conn.close()
+
+
+
+########################### Кінець ###########################
 
 
 #лове інлайн для прикріпленя групи до студента
